b1c1dff6e8806ddc0a00c53760b656a1ed062c1a
caofangsheng93 <fangsheng93@163.com>
2016-12-09 10:02:04 +0800

    水晶报表的导出

---
+++ CryStalReportInMvc/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CryStalReportInMvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CustomerDBEntities context = new CustomerDBEntities();  
+
+        public ActionResult Index()
+        {
+           var customerList= context.Customers.ToList();
+           return View(customerList);
+        }
+
+        public ActionResult ExportCustomers()
+        {
+            List<Customer> allCustomer = new List<Customer>();
+            allCustomer = context.Customers.ToList();
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/CrystalReports"), "ReportCustomer.rpt"));
+
+            rd.SetDataSource(ToDataTable<Customer>(allCustomer));
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();  
+            Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);  
+            stream.Seek(0, SeekOrigin.Begin);  
+            return File(stream, "application/pdf", "CustomerList.pdf");  
+
+        }
+
+        /// <summary>    
+        /// 将泛型集合类转换成DataTable    
+        /// </summary>    
+        /// <typeparam name="T">集合项类型</typeparam>    
+        /// <param name="list">集合</param>    
+        /// <param name="propertyName">需要返回的列的列名</param>    
+        /// <returns>数据集(表)</returns>    
+        public static DataTable ToDataTable<T>(IList<T> list, params string[] propertyName)
+        {
+            List<string> propertyNameList = new List<string>();
+            if (propertyName != null)
+                propertyNameList.AddRange(propertyName);
+            DataTable result = new DataTable();
+            if (list.Count > 0)
+            {
+                PropertyInfo[] propertys = list[0].GetType().GetProperties();
+                foreach (PropertyInfo pi in propertys)
+                {
+                    if (propertyNameList.Count == 0)
+                    {
+                        result.Columns.Add(pi.Name, pi.PropertyType);
+                    }
+                    else
+                    {
+                        if (propertyNameList.Contains(pi.Name))
+                            result.Columns.Add(pi.Name, pi.PropertyType);
+                    }
+                }
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    ArrayList tempList = new ArrayList();
+                    foreach (PropertyInfo pi in propertys)
+                    {
+                        if (propertyNameList.Count == 0)
+                        {
+                            object obj = pi.GetValue(list[i], null);
+                            tempList.Add(obj);
+                        }
+                        else
+                        {
+                            if (propertyNameList.Contains(pi.Name))
+                            {
+                                object obj = pi.GetValue(list[i], null);
+                                tempList.Add(obj);
+                            }
+                        }
+                    }
+                    object[] array = tempList.ToArray();
+                    result.LoadDataRow(array, true);
+                }
+            }
+            return result;
+        }  
+    }
+}
